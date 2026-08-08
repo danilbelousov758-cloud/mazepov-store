c7bbf838250d55126ab74ecc98bc6c93109ec881
Danil Belousov <danilbelousov758@gmail.com>
2026-08-09 01:23:57 +0300

    update mods page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,174 +1,191 @@
 import Image from "next/image";
 
-
 export default function Header() {
 
-return (
+  return (
 
-<header
-className="
-fixed
-top-6
-left-1/2
--translate-x-1/2
-z-50
+    <header
 
-w-[82%]
-max-w-5xl
+      className="
+        w-[82%]
+        max-w-5xl
+        mx-auto
+        mt-6
+        rounded-2xl
+        bg-black/90
+        border
+        border-white/20
+        px-7
+        py-4
+        flex
+        items-center
+        justify-between
+        text-white
+        shadow-xl
+        backdrop-blur-md
+      "
 
-rounded-2xl
-
-bg-black/90
-
-border
-border-white/20
-
-px-7
-py-4
-
-flex
-items-center
-justify-between
-
-text-white
-
-shadow-xl
-backdrop-blur-md
-"
->
+    >
 
 
-{/* LOGO */}
+      {/* LOGO */}
 
-<a
-href="/"
-className="
-flex
-items-center
-gap-3
-"
->
+      <a
 
-<Image
-src="/images/logo.png"
-alt="STORE"
-width={42}
-height={42}
-className="
-rounded-xl
-object-cover
-"
-/>
+        href="/"
 
+        className="
+          flex
+          items-center
+          gap-3
+        "
 
-<span
-className="
-text-xl
-font-bold
-tracking-widest
-"
->
-STORE
-</span>
+      >
+
+        <span
+
+          className="
+            text-xl
+            font-bold
+            tracking-widest
+          "
+
+        >
+
+          STORE
+
+        </span>
 
 
-</a>
+      </a>
 
 
 
 
 
-{/* MENU */}
 
-<nav
-className="
-flex
-items-center
-gap-8
-text-base
-"
->
+      {/* MENU */}
 
+      <nav
 
-<a
-href="/"
-className="
-hover:text-gray-400
-transition
-"
->
-Главная
-</a>
+        className="
+          flex
+          items-center
+          gap-6
+          text-sm
+        "
+
+      >
 
 
+        <a
 
-<a
-href="/mods"
-className="
-hover:text-gray-400
-transition
-"
->
-Моды
-</a>
+          href="/"
+
+          className="
+            hover:text-gray-400
+            transition
+          "
+
+        >
+
+          Главная
+
+        </a>
 
 
 
-<a
-href="/news"
-className="
-hover:text-gray-400
-transition
-"
->
-Новости
-</a>
+
+
+        <a
+
+          href="/mods"
+
+          className="
+            hover:text-gray-400
+            transition
+          "
+
+        >
+
+          Моды
+
+        </a>
 
 
 
-<a
-href="/support"
-className="
-hover:text-gray-400
-transition
-"
->
-Поддержка
-</a>
+
+
+        <a
+
+          href="/news"
+
+          className="
+            hover:text-gray-400
+            transition
+          "
+
+        >
+
+          Новости
+
+        </a>
 
 
 
-<a
-href="/login"
-className="
-ml-2
-px-5
-py-2
-rounded-xl
 
-border
-border-white/20
 
-bg-white/10
+        <a
 
-font-semibold
+          href="/support"
 
-hover:bg-white
-hover:text-black
+          className="
+            hover:text-gray-400
+            transition
+          "
 
-transition
-"
->
-Войти
-</a>
+        >
+
+          Поддержка
+
+        </a>
 
 
 
-</nav>
 
 
-</header>
+        <a
 
-);
+          href="/login"
+
+          className="
+            ml-2
+            px-5
+            py-2
+            rounded-xl
+            border
+            border-white/20
+            bg-white/10
+            font-semibold
+            hover:bg-white
+            hover:text-black
+            transition
+          "
+
+        >
+
+          Войти
+
+        </a>
+
+
+
+      </nav>
+
+
+
+    </header>
+
+  );
 
 }
