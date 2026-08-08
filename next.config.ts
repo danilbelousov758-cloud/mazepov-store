62c011e34f8e1b2e6aa5bffb4de6c069350d41a8
Danil Belousov <danilbelousov758@gmail.com>
2026-08-09 01:33:58 +0300

    update mods page

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,17 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
 
   experimental: {
-
-    workerThreads: false,
-
     cpus: 1,
-
   },
 
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 
 };
-
 
 export default nextConfig;
