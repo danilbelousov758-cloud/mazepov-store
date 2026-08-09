bb14e314cd4f47b482670b324d19089e6e3b2c04
Danil Belousov <danilbelousov758@gmail.com>
2026-08-09 21:21:50 +0300

    update mods page

--- app/api/mods/create/route.ts
+++ app/api/mods/create/route.ts
@@ -1,48 +1,76 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { PrismaClient } from "@prisma/client";
+import { writeFile, mkdir } from "fs/promises";
+import path from "path";
 
 
-export async function POST(req: Request){
+const prisma = new PrismaClient();
+
+
+
+export async function POST(
+req: Request
+){
 
 try{
 
-const body = await req.json();
 
-const {
-title,
-category,
-description,
-image,
-dff,
-txd,
-user
-}=body;
+const form =
+await req.formData();
 
 
-if(!user){
-return NextResponse.json(
-{
-error:"Не авторизован"
-},
-{
-status:401
-}
-);
-}
+
+const title =
+form.get("title") as string;
+
+
+
+const category =
+form.get("category") as string;
+
+
+
+const txdPath =
+form.get("txdPath") as string;
+
+
+
+const dffPath =
+form.get("dffPath") as string;
+
+
+
+const image =
+form.get("image") as File;
+
+
+
+const txd =
+form.get("txd") as File;
+
+
+
+const dff =
+form.get("dff") as File;
+
+
 
 
 
 if(
-user.role !== "OWNER" &&
-user.role !== "ADMIN"
+!title ||
+!category ||
+!image ||
+!txd ||
+!dff
 ){
 
 return NextResponse.json(
 {
-error:"Нет доступа"
+error:"Не все данные заполнены"
 },
 {
-status:403
+status:400
 }
 );
 
@@ -50,25 +78,139 @@ status:403
 
 
 
-const mod = await prisma.mod.create({
 
-data:{
 
-title,
 
-category,
 
-description,
+const uploadDir =
+path.join(
+process.cwd(),
+"public/uploads/mods"
+);
 
-image,
 
-dff,
 
-txd
+await mkdir(
+uploadDir,
+{
+recursive:true
+}
+);
+
+
+
+
+
+
+async function saveFile(
+file:File
+){
+
+
+const bytes =
+await file.arrayBuffer();
+
+
+
+const buffer =
+Buffer.from(bytes);
+
+
+
+const filename =
+Date.now()+"_"+file.name;
+
+
+
+const filepath =
+path.join(
+uploadDir,
+filename
+);
+
+
+
+await writeFile(
+filepath,
+buffer
+);
+
+
+
+return "/uploads/mods/"+filename;
+
 
 }
 
+
+
+
+
+
+
+const imageUrl =
+await saveFile(image);
+
+
+
+const txdUrl =
+await saveFile(txd);
+
+
+
+const dffUrl =
+await saveFile(dff);
+
+
+
+
+
+
+
+
+
+const mod =
+await prisma.mod.create({
+
+data:{
+
+
+title,
+
+
+category,
+
+
+image:imageUrl,
+
+
+txd:txdUrl,
+
+
+dff:dffUrl,
+
+
+txdPath:
+
+
+txdPath || "",
+
+
+
+dffPath:
+
+
+dffPath || "",
+
+
+}
+
+
 });
+
+
+
+
 
 
 
@@ -76,17 +218,21 @@ return NextResponse.json(
 {
 success:true,
 mod
-},
-{
-status:201
 }
 );
 
 
 
-}catch(error){
+}
+catch(error){
 
-console.error(error);
+
+console.error(
+"CREATE MOD ERROR:",
+error
+);
+
+
 
 return NextResponse.json(
 {
@@ -96,6 +242,8 @@ error:"Ошибка сервера"
 status:500
 }
 );
+
+
 
 }
 
